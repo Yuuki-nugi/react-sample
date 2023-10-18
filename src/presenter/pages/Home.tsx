2f50412afd647c065012b2941272699916ec4595
Yuki-Kunugi <kunuyuu3@gmail.com>
2023-10-18 18:47:49 +0900

    materialUIを適用

--- src/presenter/pages/Home.tsx
+++ src/presenter/pages/Home.tsx
@@ -1,13 +1,44 @@
+import { Box, Button, Container, Grid, styled } from '@mui/material';
 import React from 'react';
 import { Link } from "react-router-dom";
+
+const PageTitleBox = styled('div')(({ theme }) => ({
+  width: 400,
+  height: 80,
+  background: theme.palette.background.default,
+  backgroundColor: "blue",
+  color: "white",
+  textAlign: "center",
+  lineHeight: "80px",
+  fontSize: 30,
+
+}))
+
+const ToProfileButton = styled(Link)(({ theme }) => ({ 
+  width: 160,
+  height: 40,
+  marginTop: 80,
+  backgroundColor: "skyblue",
+  borderRadius: 20,
+  textAlign: "center",
+  lineHeight: "40px",
+}))
 
 const Home = () => {
   return (
     <>
-      <h1>Page2へ</h1>
-      <div>
-        <Link to={`/profile/`}>Page2</Link>
-      </div>
+      <Grid 
+        container
+        direction="column"
+        alignItems="center"
+        style={{ minHeight: '100vh' }}
+        padding={8}
+      >
+        <PageTitleBox>ホーム</PageTitleBox>
+        <ToProfileButton to={`/profile/`}>
+          <Box>プロフィールへ</Box>
+        </ToProfileButton>
+      </Grid>
     </>
   );
 };
